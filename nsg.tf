dc6cb5f9cf1bbab655c30a69b710cad17dea6d61
Y3llowm4n <mikegeelen2002@gmail.com>
2023-05-31 22:46:13 +0200

    bash edited

--- nsg.tf
+++ nsg.tf
@@ -27,6 +27,53 @@ resource "azurerm_network_security_group" "terraform-nsg" {
     destination_address_prefix = "*"
   }
 
+    security_rule {
+    name                       = "http-rule-out"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+    security_rule {
+    name                       = "sql-server-rule-out"
+    priority                   = 101
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+      security_rule {
+    name                       = "http-rule-in"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+    security_rule {
+    name                       = "sql-server-rule-in"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
   tags = {
     evironment = "production"
